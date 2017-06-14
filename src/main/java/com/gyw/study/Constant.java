c03998d58bcf8f33b01b3582342e7f3936e60448
gyw33188 <gyw33188@ly.com>
2017-06-14 17:48:22 +0800

    init

---
+++ src/main/java/com/gyw/study/Constant.java
@@ -0,0 +1,12 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package com.gyw.study;
+
+/**
+ * @author gyw33188
+ * @version $Id: Constant.java, v0.1 2017/6/14 gyw33188 Exp $$
+ */
+public abstract class Constant {
+}
